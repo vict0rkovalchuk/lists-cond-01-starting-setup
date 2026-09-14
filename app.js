3a16834caf230b5605efad93fd7b5d349496e3f9
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-14 21:05:57 +0300

    removing list items

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = Vue.createApp({
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = '';
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
     }
   }
 });
